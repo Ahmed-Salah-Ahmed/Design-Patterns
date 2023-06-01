0aff306b1c5b9f131bd689d1353eee8d0550ae97
AHMHAM <ahmedhambouta@gmail.com>
2023-06-01 17:56:13 +0300

    Factory Pattern - Simple Factory

---
+++ 04) Factory Pattern/simplefactory/SimplePizzaFactory.java
@@ -0,0 +1,25 @@
+package simplefactory;
+
+import simplefactory.pizzas.CheesePizza;
+import simplefactory.pizzas.ClamPizza;
+import simplefactory.pizzas.PepperoniPizza;
+import simplefactory.pizzas.Pizza;
+import simplefactory.pizzas.VeggiePizza;
+
+public class SimplePizzaFactory {
+
+	public Pizza createPizza(String type) {
+		Pizza pizza = null;
+
+		if (type.equals("cheese")) {
+			pizza = new CheesePizza();
+		} else if (type.equals("pepperoni")) {
+			pizza = new PepperoniPizza();
+		} else if (type.equals("clam")) {
+			pizza = new ClamPizza();
+		} else if (type.equals("veggie")) {
+			pizza = new VeggiePizza();
+		}
+		return pizza;
+	}
+}
